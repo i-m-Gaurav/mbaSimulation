3bd9cabe4ccd3a3e491c66acada145a337bc4938
Gaurav <indiagauravkumar@gmail.com>
2025-11-24 08:42:14 +0530

    url update

--- be/index.js
+++ be/index.js
@@ -13,9 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin:[
-      "http://localhost:5173",
-      "https://mba-simulation.vercel.app/"
-
+      "https://mba-simulation.vercel.app"
     ],
     
 
